3d5ee192b39ea5d522e2f527b407fb682d2ed2c5
Respect0872 <shoyzaqovasilbek@gmail.com>
2023-05-16 14:27:44 +0500

    User and Role models created and done

--- database/migrations/2023_10_12_000000_create_users_table.php
+++ database/migrations/2023_10_12_000000_create_users_table.php
@@ -9,10 +9,11 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('role_id')->default(value:2)->constrained();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
@@ -25,7 +26,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('users');
     }
